00324e01faf6da5cb891b8163add1baf41313286
aarifsk <aarifsk1618@gmail.com>
2022-10-02 06:13:36 +0900

    sample view with hardcoded text

--- NextWidget/nextEventWidget/nextEventWidget.swift
+++ NextWidget/nextEventWidget/nextEventWidget.swift
@@ -10,23 +10,23 @@ import SwiftUI
 import Intents
 
 struct Provider: IntentTimelineProvider {
-    func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date(), configuration: ConfigurationIntent())
+    func placeholder(in context: Context) -> DayEntry {
+        DayEntry(date: Date(), configuration: ConfigurationIntent())
     }
 
-    func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date(), configuration: configuration)
+    func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (DayEntry) -> ()) {
+        let entry = DayEntry(date: Date(), configuration: configuration)
         completion(entry)
     }
 
     func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-        var entries: [SimpleEntry] = []
+        var entries: [DayEntry] = []
 
         // Generate a timeline consisting of five entries an hour apart, starting from the current date.
         let currentDate = Date()
         for hourOffset in 0 ..< 5 {
             let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate, configuration: configuration)
+            let entry = DayEntry(date: entryDate, configuration: configuration)
             entries.append(entry)
         }
 
@@ -35,16 +35,28 @@ struct Provider: IntentTimelineProvider {
     }
 }
 
-struct SimpleEntry: TimelineEntry {
+struct DayEntry: TimelineEntry {
     let date: Date
     let configuration: ConfigurationIntent
 }
 
 struct nextEventWidgetEntryView : View {
-    var entry: Provider.Entry
+    var entry: DayEntry
 
     var body: some View {
-        Text(entry.date, style: .time)
+        ZStack{
+            ContainerRelativeShape().fill(.gray.gradient)
+            VStack{
+                HStack(spacing: 4){
+                    Text("❄️").font(.title)
+                    Text(entry.date.weekdayDisplayFormat).font(.title3).fontWeight(.bold).minimumScaleFactor(0.7).foregroundColor(.black.opacity(0.7))
+                    Spacer()
+                }
+                Text(entry.date.dayDisplayFormat).font(.system(size: 80, weight: .heavy)).foregroundColor(.white.opacity(0.6))
+            }
+            .padding()
+        }
+        
     }
 }
 
@@ -63,7 +75,16 @@ struct nextEventWidget: Widget {
 
 struct nextEventWidget_Previews: PreviewProvider {
     static var previews: some View {
-        nextEventWidgetEntryView(entry: SimpleEntry(date: Date(), configuration: ConfigurationIntent()))
+        nextEventWidgetEntryView(entry: DayEntry(date: Date(), configuration: ConfigurationIntent()))
             .previewContext(WidgetPreviewContext(family: .systemSmall))
+    }
+}
+
+extension Date {
+    var weekdayDisplayFormat: String {
+        self.formatted(.dateTime.weekday(.wide))
+    }
+    var dayDisplayFormat: String {
+        self.formatted(.dateTime.day())
     }
 }
